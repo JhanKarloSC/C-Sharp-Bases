ffff1f8d05834cd48169e73f9590ec4fa6a785d5
Jhan Karlo Santamaria Calderón <jhankarlo.smc@gmail.com>
2023-03-07 11:44:46 -0500

    Declaración 2

---
+++ C-Sharp-Bases/1-Variables/Declaracion.cs
@@ -0,0 +1,57 @@
+﻿namespace C_Sharp_Bases.Variables
+{
+    public class Declaracion
+    {
+        void Explisita()
+        {
+            //int edadPersona;
+            //string nombrePersona;
+            //bool mayorDeEdad;
+
+            //edadPersona = 30;
+            //nombrePersona = "Jhan Karlo";
+            //mayorDeEdad = true;
+
+            int edadPersona = 30;
+            string nombrePersona = "Jhan Karlo";
+            bool mayorDeEdad = true;
+
+            Console.WriteLine(edadPersona);
+            Console.WriteLine(nombrePersona);
+            Console.WriteLine(mayorDeEdad);
+
+            //int 1Variables = ""; //no es válido
+            //int @variable@ = ""; //no es válido
+            //int double = 0; //no es válido
+            //int string = ""; //no es válido
+            //int variable-con-guio = ""; //no es válido
+
+            int miVariable = 0;//Camel Case recomendado
+        }
+
+        void Implisita()
+        {
+            var numeros = 0;
+            var texto = "";
+            var fecha = DateTime.Now;
+            var dinero = 9.99m;
+        }
+
+        void ValoresPorDefecto()
+        {
+            int entero = default; //Por defecto 0
+            Console.WriteLine(entero);
+
+            int entero2 = default(int);
+            Console.WriteLine(entero2);
+
+            bool esVerdaderoONo = default;
+            Console.WriteLine(esVerdaderoONo);
+
+            decimal decimal1 = default;
+            Console.WriteLine(decimal1);
+            DateTime dateTime1 = default;
+            Console.WriteLine(dateTime1);
+        }
+    }
+}
