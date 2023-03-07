9ef0ce800b976fbcadf1904ea791e3bbd9a6a82b
Jhan Karlo Santamaria Calderón <jhankarlo.smc@gmail.com>
2023-03-07 12:16:52 -0500

    tipos de valor o referencia

---
+++ C-Sharp-Bases/1-Variables/TipoValorReferencia.cs
@@ -0,0 +1,14 @@
+﻿namespace C_Sharp_Bases._1_Variables;
+
+internal class TipoValorReferencia
+{
+    void valor()
+    {
+        int valor = 0;
+    }
+
+    void referencias()
+    {
+        int? valor = null;
+    }
+}
