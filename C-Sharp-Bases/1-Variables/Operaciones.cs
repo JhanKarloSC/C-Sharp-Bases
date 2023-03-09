a621ee80ae8bd0924fdbcdfe5d4664041f072304
Jhan Karlo Santamaria Calderón <jhankarlo.smc@gmail.com>
2023-03-09 09:05:48 -0500

    Operaciones

---
+++ C-Sharp-Bases/1-Variables/Operaciones.cs
@@ -0,0 +1,17 @@
+﻿namespace C_Sharp_Bases._1_Variables
+{
+    internal class Operaciones
+    {
+        public Operaciones()
+        {
+            var numero1 = 30;
+            var numero2 = 20;
+
+            var suma = numero1 + numero2;
+            var resta = numero1 - numero2;
+            var multiplicacion = numero1 * numero2;
+            var division = numero1 / numero2;
+            var residuo = numero1 % numero2;
+        }
+    }
+}
