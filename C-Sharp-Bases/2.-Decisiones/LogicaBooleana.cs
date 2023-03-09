c9b29ff2317b885bc327bdef963ba99771c44ab3
Jhan Karlo Santamaria Calderón <jhankarlo.smc@gmail.com>
2023-03-09 15:23:49 -0500

    Decisiones

---
+++ C-Sharp-Bases/2.-Decisiones/LogicaBooleana.cs
@@ -0,0 +1,21 @@
+﻿namespace C_Sharp_Bases._2._Decisiones
+{
+    internal class LogicaBooleana
+    {
+        public LogicaBooleana()
+        {
+            var numero1 = 10;
+            var numero2 = 5;
+
+            var numero1IgualCantidad2 = numero1 == numero2;
+            var numero1DiferenteCantidad2 = numero1 != numero2;
+
+            var numero1MayorNumero2 = numero1 > numero2;
+            var numero1MenorNumero2 = numero1 < numero2;
+
+            var numero1MayorIgualNumero2 = numero1 >= numero2;
+            var numero1MenorIgualNumero2 = numero1 <= numero2;
+
+        }
+    }
+}
---
+++ C-Sharp-Bases/2.-Decisiones/Operadores.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Bases._2._Decisiones
+{
+    internal class Operadores
+    {
+        public Operadores()
+        {
+            //Boleano
+            var numero = 10;
+
+            var esValido = numero > 5 && numero < 20;
+            var esValido2 = numero > 5 || numero == 20;
+
+            var nombre = "jhankarlo";
+            var esIgual = nombre == "jhankarlo";
+
+            //true && true => true
+            //true && false => false
+            //false && true => false
+            //false && false => false
+
+            //true || true => true
+            //true || false => true
+            //false || true => true
+            //false || false => false
+        }
+
+        public void sentenciaIf()
+        {
+            var numero = 10;
+
+            if (numero > 0)
+            {
+                Console.WriteLine("Es mayor que 0");
+            }
+
+            if (numero > 0)
+                Console.WriteLine("Es mayor que 0");
+
+            if (numero > 0)
+            {
+                Console.WriteLine("Es mayor que 0");
+            }
+            else
+            {
+                Console.WriteLine("Es menor que 0");
+            }
+
+            if (numero > 0)
+                Console.WriteLine("Es mayor que 0");
+            else
+                Console.WriteLine("Es menor que 0");
+
+            if (numero == 0)
+            {
+                Console.WriteLine("Es igual a 0");
+            }
+            else if (numero > 10)
+            {
+                Console.WriteLine("Es mayor a 10");
+            }
+            else if (numero > 20)
+            {
+                Console.WriteLine("Es mayor a 20");
+            }
+            else
+            {
+                Console.WriteLine("Condición por defecto");
+            }
+        }
+
+        void Ternario()
+        {
+            var numero = 11;
+            var esValido = numero > 10 ? "Si es mayor" : "No es mayor";
+        }
+
+        void sentenciaSwitch()
+        {
+            var posicion = 10;
+
+            switch (posicion)
+            {
+                case 1:
+                    Console.WriteLine("Lugar 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Lugar 2");
+                    break;
+                case 3:
+                    Console.WriteLine("Lugar 3");
+                    break;
+                default:
+                    Console.WriteLine("No ganaste");
+                    break;
+            }
+        }
+
+        void patronesRelacionales()
+        {
+            var edad = 31;
+
+            var mensaje = edad switch
+            {
+                >= 18 => "Mayor edad",
+                <= 17 => "Menor edad"
+            };
+        }
+
+        void patronesLogicos()
+        {
+            string? nombre = "Jhan Karlo";
+
+            if (nombre is not null)
+            {
+                Console.WriteLine(nombre.ToUpper());
+            }
+
+            var edad = 18;
+            var mensaje = edad switch
+            {
+                > 0 and < 18 => "Menor edad",
+                >= 18 => "Mayor edad"
+            };
+        }
+    }
+}
